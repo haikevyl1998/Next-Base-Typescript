8b2124a8fb7fc63398941820e23daf24d0d182fc
Hai Nguyen <nguyen.van.hai-b@sun-asterisk.com>
2023-03-22 23:52:02 +0700

    add chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '@/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} key={router.asPath} />
+    </ChakraProvider>
+  );
 }
