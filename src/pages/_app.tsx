6d58b4a97feec0b1c9668a754e549bbe4d18a4e5
Hai Nguyen <nguyen.van.hai-b@sun-asterisk.com>
2023-03-22 20:49:52 +0700

    add react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,27 @@
 import type { AppProps } from 'next/app';
+import { useRef } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClientRef = useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          staleTime: 10 * 1000,
+        },
+      },
+    });
+  }
+
+  return (
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />;
+      </Hydrate>
+      {process.env.NODE_ENV !== 'production' && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  );
 }
